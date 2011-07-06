5434d0979edc2510cab76994bb8fe502ad1c76af
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2011-07-06 17:06:16 +0200

    wip

--- source/class/desk/Application.js
+++ source/class/desk/Application.js
@@ -19,136 +19,51 @@
  */
 qx.Class.define("desk.Application",
 {
-  extend : qx.application.Standalone,
-  //extend : qx.application.Inline,
+	extend : qx.application.Standalone,
+	//extend : qx.application.Inline,
 
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
+	/*
+	*****************************************************************************
+	MEMBERS
+	*****************************************************************************
+	*/
 
-  members :
-  {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
-     */
-
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
-
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
-
-      var win = new qx.ui.window.Window("Files");
-//      win.setPadding(10);
-      win.setLayout(new qx.ui.layout.VBox());
-		win.setAllowClose(false);
-		win.setAllowMinimize(false);
-      win.open();
-      var doc=this.getRoot();
-     this.getRoot().add(win);
-
-      var tree = new qx.ui.tree.Tree().set({width : 400, height : 400 });
-
-      var root = new qx.ui.tree.TreeFolder("data");
-      root.setOpen(true);
-      tree.setRoot(root);
-      root.setUserData("parent_directory","");
-      win.add(tree);
-
-	function getNodeURL(node)
+	members :
 	{
-		return ("http://vip.creatis.insa-lyon.fr:8080/visu/"+getNodePath(node));
-	}
+		/**
+		* This method contains the initial application code and gets called 
+		* during startup of the application
+		* 
+		* @lint ignoreDeprecated(alert)
+		*/
 
-	function getNodePath(node)
-	{
-		if (node.getLabel()=="data")
-			return ("data");
-		else
+		main : function()
 		{
-			var parent=node.getParent().getUserData("parent_directory");
-			if (parent=="")
-				return (node.getParent().getLabel()+"\/"+node.getLabel());
-			else			
-				return (node.getParent().getUserData("parent_directory")+
-					"\/"+
-					node.getParent().getLabel()+
-					"\/"+
-					node.getLabel());
+			// Call super class
+			this.base(arguments);
+
+			// Enable logging in debug variant
+			if (qx.core.Environment.get("qx.debug"))
+			{
+				// support native logging capabilities, e.g. Firebug for Firefox
+				qx.log.appender.Native;
+				// support additional cross-browser console. Press F7 to toggle visibility
+				qx.log.appender.Console;
+			}
+
+			function fileClicked(file) {
+				var extension=file.substring(file.length-4, file.length);
+				if ((extension==".vtk")||(extension==".xml"))
+				{
+				var meshView=new desk.meshView(file);
+				//this.getRoot().add(meshView);
+				}
+			}
+
+			var myBrowser=new desk.fileBrowser();
+			myBrowser.setFileHandler(fileClicked);
+			this.getRoot().add(myBrowser);
+
 		}
 	}
-
-	function fileClicked(node) {
-		var file=getNodeURL(node);
-		var extension=file.substring(file.length-4, file.length);
-		if ((extension==".vtk")||(extension==".xml"))
-		{
-			var meshView=new desk.meshView(file);
-			doc.add(meshView);
-		}
-	}
-
-	function expandDirectoryListing(node) {
-	node.removeAll();
-	 var ajax = new XMLHttpRequest();
-		ajax.onreadystatechange = function()
-		{
-				if(this.readyState == 4 && this.status == 200)
-				{
-					//	alert("ajax.responseText : " + ajax.responseText);
-						var files=ajax.responseText.split("\n");
-					//	alert (files.length+" files");
-						for (var i=0;i<files.length;i++)
-						{
-							var splitfile=files[i].split(" ");
-							if (splitfile[0]!="")
-							{
-								if (splitfile[1]=="file")
-								{
-									var filenode=new qx.ui.tree.TreeFile(splitfile[0]);
-									node.add(filenode);
-									filenode.addListener("click", function(event){fileClicked(this);},directorynode);
-								}
-								else
-								{
-									var directorynode=new qx.ui.tree.TreeFolder(splitfile[0]);
-									node.add(directorynode);
-									directorynode.setUserData("parent_directory", getNodePath(node));
-									directorynode.addListener("click", function(event){expandDirectoryListing(this);},
-											directorynode);
-								}
-							}
-						}
-				}
-				else if (this.readyState == 4 && this.status != 200)
-				{
-						// fetched the wrong page or network error...
-						alert('"Fetched the wrong page" OR "Network error"');
-				}
-		};
-		ajax.open("POST", "/visu/listdir.php", false);
-		ajax.send(getNodePath(node));
-	 }
-
-     expandDirectoryListing(root);
-     root.addListener("click", function(event){
-			        		            expandDirectoryListing(this);
-       							     },root);
-
-//	displayMesh("http://vip.creatis.insa-lyon.fr:8080/visu/meshView/ADAM/adam.xml");
-    }
-  }
 });
--- source/class/desk/meshView.js
+++ source/class/desk/meshView.js
@@ -6,22 +6,30 @@ qx.Class.define("desk.meshView",
 	{
 		this.base(arguments);
 
-		var layout = new qx.ui.layout.VBox();
-		this.setLayout(layout);
-		this.setAllowClose(true);
-		this.setAllowMinimize(false);
+		this.setLayout(new qx.ui.layout.VBox());
+		this.setShowClose(true);
+		this.setShowMinimize(false);
 		this.setResizable(true,true,true,true);
+		this.setUseResizeFrame(true);
+		this.setUseMoveFrame(true);
 		this.setContentPadding(2);
+		this.setCaption(file);
 		this.open();
 
 		this.__iframe = new qx.ui.embed.Iframe().set({
 			width: 400,
 			height: 400,
-			decorator:null,
-			source: "http://vip.creatis.insa-lyon.fr:8080/visu/meshView/"+"?"+file
-			});
+			minWidth: 200,
+			minHeight: 150,
+			decorator:null});
+		
+		this.__iframe.setFrameName('o3dframe');
+		this.__iframe.setSource("http://vip.creatis.insa-lyon.fr:8080/visu/meshView/"+"?"+file);
 		this.add(this.__iframe,{flex: 1});
 		this.addListener("mouseout", function(event) {this.__iframe.getWindow().g_scene.stopDragging();},this);
+		this.addListener("keypress", function(event) {
+			this.__iframe.getWindow().keyPressed(event.getKeyCode());},this);
+		return (this);
 	},
 
 	members : {
