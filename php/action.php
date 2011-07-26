794a9a5b97ffadfea4e854cbe9eceef2a47791d4
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2011-07-26 15:14:53 +0200

    update action.php

--- php/action.php
+++ php/action.php
@@ -1,31 +1,144 @@
 <?php
-$actions = simplexml_load_file("desk/actions.xml") or die("
-Fichier introuvable. L'analyse a ete suspendue");
 
-$tabulation="&nbsp;&nbsp;&nbsp;";
+$DATA_ROOT_FROM_PHP="data/";
+$DIR_TO_PHP="/var/www/html/visu/";
 
-$actionToPerform=mysql_real_escape_string($_POST["action"]);
-echo "action : $actionToPerform\n";
-$nvertices=mysql_real_escape_string($_POST["number_of_desired_vertices"]);
-echo "nvertices : $nvertices\n";
-/*
+
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+	die ("\n error while processing\n");
+}
+//set_error_handler("myErrorHandler");
+
+function validatePath($file) {
+	$begining=substr($file, 0, strlen($DATA_ROOT_FROM_PHP));
+	if ($begining!=$DATA_ROOT_FROM_PHP)
+		die ("bad directory : $file\n".
+				"begins with \"$begining\"\n".
+				"must begin with \"$DATA_ROOT_FROM_PHP\"");
+}
+
+$actions = simplexml_load_file("desk/actions.xml")
+	or die("Fichier introuvable. L'analyse a ete suspendue");
+
+$actionToPerform=mysql_real_escape_string($_POST["action"])
+	or die ("no action asked!");
+
+//echo "action : $actionToPerform\n";
+
 foreach ($actions->children() as $action)
 {
-	foreach($action->attributes() as $a => $b)
+	if ($action->getName()=="action")
 	{
-		if ($a=="name")
-			echo "action : ",$b,"<br/>";
-	}
-	foreach ($action->children() as $parameter)
-	{
-		echo "$tabulation parameter: " . $parameter . "<br/>";
-		foreach($parameter->attributes() as $a => $b)
+		$currentActionName=$action["name"]
+			or die ("no name given for one action in xml file");
+
+		if ($actionToPerform==$currentActionName)
 		{
-			echo "$tabulation $tabulation",$a,'=',$b,"<br>";
+			$command=$action["executable"]
+				or die("no executable provided for action \"$actionToPerform\"");
+			// action was found in xml file, let's parse the parameters
+
+			// first add mandatory output directory parameter
+			$outputPirectoryParameter = $action->addChild('parameter');
+			$outputPirectoryParameter->addAttribute('name', "output_directory");
+			$outputPirectoryParameter->addAttribute('type', "directory");
+			$outputPirectoryParameter->addAttribute('required', "true");
+			$outputDirectory="";
+
+			foreach ($action->children() as $parameter)
+			{
+				if ($parameter->getName()=="parameter")
+				{
+					$parameterName=$parameter["name"];
+//					echo $parameterName,"\n";
+					$parameterType=$parameter["type"];
+					if ($parameter["required"]=="true")
+					{
+						$try=$_POST[''.$parameterName];
+						$parameterValue=mysql_real_escape_string($try)
+							or die ("parameter $parameterName is required for the server\n".
+							$try);
+					}
+
+					switch ($parameterType)
+					{
+						case "file":
+							validatePath($parameterValue);
+							if (!is_file($parameterValue))
+								die ("$parameterName : file \"$parameterValue\" does not exist");
+							$parameterValue="$DIR_TO_PHP$parameterValue";
+							break;
+						case "directory":
+							validatePath($parameterValue);
+							if (!is_dir($parameterValue))
+								die ("$parameterName : directory \"$parameterValue\" does not exist");
+							$parameterValue="$DIR_TO_PHP$parameterValue";
+							break;
+						case "int":
+							if (!ctype_digit("$parameterValue"))
+								die ("$parameterName : value \"$parameterValue\" is not an integer value");
+							$value=floatVal($parameterValue);
+							$min=$parameter["min"];
+							if ($min!="")
+							{
+								$min=floatVal($min);
+								if ($min>$value)
+									die ("$parameterName : value $parameterValue should be bigger than $min");
+							}
+							$max=$parameter["max"];
+							if ($max!="")
+							{
+								$max=floatVal($max);
+								if ($max<$value)
+									die ("$parameterName : value $parameterValue should be smaller than $max");
+							}
+							break;
+						case "float":
+							if (!is_numeric($parameterValue))
+								die ("$parameterName : value \"$parameterValue\" is not a number");
+							$value=floatVal($parameterValue);
+							$min=$parameter["min"];
+							if ($min!="")
+							{
+								$min=floatVal($min);
+								if ($min>$value)
+									die ("$parameterName : value $parameterValue should be bigger than $min");
+							}
+							$max=$parameter["max"];
+							if ($max!="")
+							{
+								$max=floatVal($max);
+								if ($max<$value)
+									die ("$parameterName : value $parameterValue should be smaller than $max");
+							}
+							break;
+						default :
+							die ("no handler for type $parameterType");
+					}
+					$prefix=$parameter["prefix"];
+					if ($prefix!="")
+						$command.=" ".$prefix;
+
+					if ($parameterName=="output_directory")
+						$outputDirectory=$parameterValue;
+					else
+						$command.=" ".$parameterValue;
+				}
+				else
+				{
+					if ($parameter->getName()=="anchor")
+					{
+						$command.=" ".$parameter["text"];
+					}
+				}
+			}
+
+			echo "chdir :$outputDirectory\n";
+			chdir ($outputDirectory);
+			echo "command : $command\n";
+//			system("$command");
 		}
 	}
-	echo "<br/>";
 }
-*/
 ?>
 
