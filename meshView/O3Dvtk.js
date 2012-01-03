599012c171fb716a61586d34f39b5fe3f9db419c
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2012-01-03 12:32:40 +0100

    bugfix

--- meshView/O3Dvtk.js
+++ meshView/O3Dvtk.js
@@ -928,7 +928,7 @@ function createFromFile3(xmlhttp, scene, file,color) {
 
 			if (index2==connectivity[0]+1)
 			{
-				var triangle;//=[0,0,0];
+				var triangle=[0,0,0];
 				index2=0;
 				var numberOfTrianglesInCell=connectivity[0]-2;
 				var vertex1=triangle[0]=connectivity[1];
