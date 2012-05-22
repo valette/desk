856df9cad0451316f7cb53cb23ea38bcb192efdd
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2012-05-22 14:31:16 +0200

    listDir working

--- server.js
+++ server.js
@@ -3,87 +3,130 @@ var libpath = require('path'),
     fs = require('fs'),
     url = require("url"),
     mime = require('mime'),
-    qs = require('querystring');
+    qs = require('querystring'),
+	async = require('async');
 
 var path = "build";
 var port = 1337;
 
-http.createServer(function (request, response) {
+var getDirectory=function (dir, callback) {
+	var realFiles=[];
 
-	if (request.method == 'POST') {
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-        });
-        request.on('end', function () {
+	fs.readdir(dir, function (err, files) {
+		if (err) {
+			callback (err);
+			return;
+		}
+		
+		for (var i=0;i!=files.length;i++) {
+			realFiles.push(dir+"/"+files[i]);
+		}
 
-            var POST = qs.parse(body);
-            // use POST
+		async.map(realFiles, fs.stat, function(err, results){
+			for (var i=0;i!=files.length;i++) {
+				results[i].name=files[i];
+			}
+			callback (err, results);
+		});
+	});
+}
 
-			fs.readdir(POST.dir, function (err, files) {
-				response.writeHead(200, {
-					"Content-Type": "text/plain"
-				});
-				for (var i=0;i!=files.length;i++)
-				{
-					response.write(files[i]+" file 10 10");
-					if (i!=files.length-1) {
-						response.write("\n");
+var createServer=function () {
+	http.createServer(function (request, response) {
+
+		if (request.method == 'POST') {
+		    var body = '';
+		    request.on('data', function (data) {
+		        body += data;
+		    });
+		    request.on('end', function () {
+
+		        var POST = qs.parse(body);
+		        // use POST
+
+				getDirectory(path+"/php/"+POST.dir, function (err, files) {
+					if (err) {
+						response.writeHead(500, {
+						  "Content-Type": "text/plain"
+						});
+						response.write(err + "\n");
+						response.end();
+						return;
 					}
-				}
-				response.end();
+					response.writeHead(200, {
+						"Content-Type": "text/plain"
+					});
+					for (var i=0;i!=files.length;i++)
+					{
+						var file=files[i];
+						var dirString;
+						if (file.isDirectory()) {
+							dirString="dir";
+						}
+						else {
+							dirString="file";
+						}
 
-			});
-        });
-        return;
-    }
+						response.write(file.name+" "+dirString+" "+file.mtime.getTime()+" "+file.size);
+						if (i!=files.length-1) {
+							response.write("\n");
+						}
+					}
+					response.end();
 
-    var uri = url.parse(request.url).pathname;
-    var filename = libpath.join(path, uri);
+				});
+		    });
+		    return;
+		}
 
-    libpath.exists(filename, function (exists) {
-        if (!exists) {
-            response.writeHead(404, {
-                "Content-Type": "text/plain"
-            });
-            response.write("404 Not Found\n");
-            response.end();
-            return;
-        }
+		var uri = url.parse(request.url).pathname;
+		var filename = libpath.join(path, uri);
 
-		fs.stat(filename, function (err, stats) {
-          if (err) {
-              response.writeHead(500, {
-                  "Content-Type": "text/plain"
-              });
-              response.write(err + "\n");
-              response.end();
-              return;
-          }
+		libpath.exists(filename, function (exists) {
+		    if (!exists) {
+		        response.writeHead(404, {
+		            "Content-Type": "text/plain"
+		        });
+		        response.write("404 Not Found\n");
+		        response.end();
+		        return;
+		    }
 
-          if (stats.isDirectory()) {
-              filename += '/index.html';
-          }
+			fs.stat(filename, function (err, stats) {
+		      if (err) {
+		          response.writeHead(500, {
+		              "Content-Type": "text/plain"
+		          });
+		          response.write(err + "\n");
+		          response.end();
+		          return;
+		      }
 
-          fs.readFile(filename, "binary", function (err, file) {
-              if (err) {
-                  response.writeHead(500, {
-                      "Content-Type": "text/plain"
-                  });
-                  response.write(err + "\n");
-                  response.end();
-                  return;
-              }
+		      if (stats.isDirectory()) {
+		          filename += '/index.html';
+		      }
 
-              var type = mime.lookup(filename);
-              response.writeHead(200, {
-                  "Content-Type": type
-              });
-              response.write(file, "binary");
-              response.end();
-          });
-        });
-    });
-}).listen(port);
+		      fs.readFile(filename, "binary", function (err, file) {
+		          if (err) {
+		              response.writeHead(500, {
+		                  "Content-Type": "text/plain"
+		              });
+		              response.write(err + "\n");
+		              response.end();
+		              return;
+		          }
 
+		          var type = mime.lookup(filename);
+		          response.writeHead(200, {
+		              "Content-Type": type
+		          });
+		          response.write(file, "binary");
+		          response.end();
+		      });
+		    });
+		});
+	}).listen(port);
+};
+
+createServer();
 console.log ("server running on port "+port+", serving path "+path);
