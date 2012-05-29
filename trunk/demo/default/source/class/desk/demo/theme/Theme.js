fab323c15f33321685df6ac6e1329427ce2c85ff
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2012-05-29 14:25:13 +0200

    wip

---
+++ trunk/demo/default/source/class/desk/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("desk.demo.theme.Theme",
+{
+  meta :
+  {
+    color : desk.demo.theme.Color,
+    decoration : desk.demo.theme.Decoration,
+    font : desk.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : desk.demo.theme.Appearance
+  }
+});
