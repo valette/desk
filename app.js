baef432939a59e188d991c2b7982e23d22f3e958
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2012-07-16 17:43:07 +0200

    app.js : basic authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var fs      = require('fs'),
 
 var app;
 
+var passwordFile="./password.json";
 var privateKeyFile="privatekey.pem";
 var certificateFile="certificate.pem";
 var separator="********************************************************************************";
@@ -16,19 +17,40 @@ path=fs.realpathSync(path)+"/";
 
 console.log(separator);
 
+// look for password.json file.
+if (fs.existsSync(passwordFile)) {
+	var identity=require(passwordFile);
+	console.log("Using basic authentication");
+	console.log(separator);
+} else {
+	console.log("No password file "+passwordFile+" provided");
+	console.log("see "+passwordFile+".example file for an example");
+	console.log(separator);
+	var identity=null;
+}
+
+function authorize(username, password) {
+	if (identity) {
+	    return identity.username === username & identity.password === password;
+    }
+    else {
+    	return true;
+    }
+}
+
 // run the server in normal or secure mode depending on provided certificate
 if (fs.existsSync(privateKeyFile) && fs.existsSync(certificateFile)) {
 	var privateKey = fs.readFileSync(privateKeyFile).toString();
 	var certificate = fs.readFileSync(certificateFile).toString();
 	app = express.createServer({key: privateKey, cert: certificate});
-	console.log("using secure https mode");
+	console.log("Using secure https mode");
 	baseURL="https://";
 }
 else {
 	app = express.createServer();
-	console.log("no certificate provided, using non secure mode");
+	console.log("No certificate provided, using non secure mode");
 	baseURL="http://";
-	console.log("you can generate a certificate with these 3 commands:");
+	console.log("You can generate a certificate with these 3 commands:");
 	console.log("(1) openssl genrsa -out privatekey.pem 1024");
 	console.log("(2) openssl req -new -key privatekey.pem -out certrequest.csr");
 	console.log("(3) openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem");
@@ -37,6 +59,7 @@ console.log(separator);
 
 //configure server : static file serving, errors
 app.configure(function(){
+  app.use(express.basicAuth(authorize)),
   app.use(express.methodOverride());
   app.use(express.bodyParser());
   app.use(express.static(path));
