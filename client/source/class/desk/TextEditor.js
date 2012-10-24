52c27ee58cc0f370e0adc51589b09098d4ba6be3
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2012-10-24 23:59:29 +0200

    textEditor : allow .js files execution

--- client/source/class/desk/TextEditor.js
+++ client/source/class/desk/TextEditor.js
@@ -17,22 +17,35 @@ qx.Class.define("desk.TextEditor",
 		this.__reloadButton = new qx.ui.form.Button("Reload");
 		this.__reloadButton.addListener("execute", function(e) {
 			this.openFile(this.__file);
-			}, this);
+		}, this);
 
 		var saveButton = new qx.ui.form.Button("Save");
 		saveButton.addListener("execute", function(e) {
 			saveButton.setEnabled(false);
 			desk.FileSystem.writeFile(this.__file, this.__textArea.getValue(),
 				function () {saveButton.setEnabled(true);});
-			}, this);
+		}, this);
+
+		this.__executeButton = new qx.ui.form.Button("execute");
+		this.__executeButton.addListener("execute", function(e) {
+			eval('(function () {' + this.__textArea.getValue() + '\n})()');
+		}, this);
+
+		var spinner = new qx.ui.form.Spinner(5, 15, 50);
+		spinner.addListener('changeValue', function (e) {
+			this.__textArea.setFont(qx.bom.Font.fromString(e.getData() + ' serif'));
+		}, this);
 
 		var buttonsContainer = new qx.ui.container.Composite;
 		buttonsContainer.setLayout(new qx.ui.layout.HBox());
+		buttonsContainer.add(this.__executeButton, {flex : 1});
 		buttonsContainer.add(this.__reloadButton, {flex : 1});
-		buttonsContainer.add(saveButton);
+		buttonsContainer.add(saveButton, {flex : 1});
+		buttonsContainer.add (spinner);
 		this.add(buttonsContainer);
 
 		this.__textArea = new qx.ui.form.TextArea();
+		this.__textArea.setFont(qx.bom.Font.fromString("15 serif"));
 		this.add(this.__textArea,{flex : 1});
 		this.open();
 
@@ -44,9 +57,17 @@ qx.Class.define("desk.TextEditor",
 		__textArea : null,
 		__file : null,
 		__reloadButton : null,
+		__executeButton : null,
 
 		openFile : function (file)
 		{
+			if (file.substring(file.length - 3) === '.js') {
+				this.__executeButton.setVisibility('visible');
+			}
+			else {
+				this.__executeButton.setVisibility('excluded');
+			}
+
 			this.__file = file;
 			this.__reloadButton.setEnabled(false);
 			desk.FileSystem.readFile(file, function (request){
