9758ff511aaba142db05bebe400c4bf6e1601c58
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2013-07-06 22:25:45 +0200

    cleanup

--- server/proxy/proxy.js
+++ server/proxy/proxy.js
@@ -1,18 +1,22 @@
 var fs = require('fs'),
     http = require('http'),
-    httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy'),
+    exec  = require('child_process').exec,
+    async = require('async');
+
+var usersFile = __dirname + '/users.json';
 
 var options = {
 	https: {
 		key: fs.readFileSync(__dirname+'/privatekey.pem', 'utf8'),
 		cert: fs.readFileSync(__dirname+'/certificate.pem', 'utf8')
 	},
-	router: __dirname+'/routes.json'
+	router: {}
 };
 
 var proxyServer = httpProxy.createServer(options);
-var port = process.env.LISTEN_PID > 0 ? 'systemd' : 8081;
-console.log('desk-proxy service listening on port '+port);
+var port = 8081;
+console.log('desk-proxy service listening on port ' + port);
 proxyServer.listen(port);
 
 var server = http.createServer(function (req, res) {
@@ -22,4 +26,33 @@ var server = http.createServer(function (req, res) {
 });
 server.listen(8080);
 console.log('desk-http2https service listening on port 8080');
+
+var routes;
+fs.watchFile(usersFile, updateRoutes);
+
+function updateRoutes() {
+	console.log(new Date());
+	console.log(usersFile + ' modified, updating routes...');
+	var users = JSON.parse(fs.readFileSync(usersFile)).users;
+	routes = {};
+
+	async.forEachSeries(users, addUser, function () {
+		proxyServer.proxy.proxyTable.setRoutes(routes);
+		console.log('... done! Routes:');
+		console.log(routes);
+	});
+}
+
+console.log('Watching file ' + usersFile + ' for routes');
+
+function addUser(user, callback) {
+        exec('id -u ' + user, function (err, stdout) {
+                var UID = parseInt(stdout, 10);
+                routes['desk.creatis.insa-lyon.fr/' + user] =
+                   'desk.creatis.insa-lyon.fr:' + UID + '/' + user;
+                callback();
+        });
+}
+
+updateRoutes();
 
