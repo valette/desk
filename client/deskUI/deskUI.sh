33e9e5ff29d71e814222a75ba5c0185bc182d6b6
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2014-03-07 16:03:34 +0100

    deskUI fixes

--- client/deskUI/deskUI.sh
+++ client/deskUI/deskUI.sh
@@ -1,3 +1,2 @@
 ./generate.py build
 cat build/script/deskui.js  ../ext/async.min.js  ../ext/three.js/Detector.js ../ext/three.js/three.min.js ../ext/underscore-min.js ../ext/operative.min.js  ../ext/three.js/TrackballControls2.js  ../ext/three.js/VTKLoader.js ../ext/three.js/ctm/CTMLoader.js  > build/script/deskUI.js
-cat source/script/deskui.js  ../ext/async.min.js  ../ext/three.js/Detector.js ../ext/three.js/three.min.js ../ext/underscore-min.js ../ext/operative.min.js  ../ext/three.js/TrackballControls2.js  ../ext/three.js/VTKLoader.js ../ext/three.js/ctm/CTMLoader.js  > source/script/deskUI.js
