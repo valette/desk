158a5aa42d8b3c39f1748baca0db46c62d1d7d69
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2014-07-28 14:56:01 +0200

    added deskUI.js (browserify code)

---
+++ server/deskUI.js
@@ -0,0 +1,13 @@
+/**
+ * Client dependencies injected via browserify
+ */
+require ('../client/deskUI/build/script/deskui.js')
+window.qx = qx;
+
+async    = require('async');
+_        = require('underscore');
+
+Detector = require('../client/ext/three.js/Detector.js');
+THREE = require('../client/ext/three.js/three.js');
+require('../client/ext/three.js/VTKLoader.js');
+require('../client/ext/three.js/TrackballControls2.js');
