e92ff3180ddcafa01b62f199209034df0384a783
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2014-08-27 11:32:29 +0200

    rework main Application script

--- client/application/source/class/desk/demo/Application.js
+++ client/application/source/class/desk/demo/Application.js
@@ -9,7 +9,6 @@
 ************************************************************************ */
 
 /**
- * This is the main application class of your custom application "desk"
  * @asset(desk/*)
  * @use(qxjqplot.Plot)
  */
@@ -20,20 +19,13 @@ qx.Class.define("desk.demo.Application",
 
 	members :
 	{
-		/**
-		* This method contains the initial application code and gets called 
-		* during startup of the application
-		* 
-		* @lint ignoreDeprecated(alert)
-		*/
 
 		main : function() {
 			// Call super class
 			this.base(arguments);
 
 			// Enable logging in debug variant
-			if (qx.core.Environment.get("qx.debug"))
-			{
+			if (qx.core.Environment.get("qx.debug")) {
 				// support native logging capabilities, e.g. Firebug for Firefox
 				qx.log.appender.Native;
 				// support additional cross-browser console. Press F7 to toggle visibility
@@ -42,58 +34,55 @@ qx.Class.define("desk.demo.Application",
 
 			function getParameter( parameterName ) {
 				parameterName = parameterName.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-				var regexS = "[\\?&]"+parameterName+"=([^&#]*)";
-				var regex = new RegExp( regexS );
+				var regex = new RegExp( "[\\?&]" + parameterName + "=([^&#]*)" );
 				var results = regex.exec( window.location.href );
-				if( results == null ) {
+				if (results == null) {
 					return null;
 				} else {
 					return results[1];
 				}
 			}
 
-			var initScript = 'code/init.js'
-			// first try to automatically launch startup script if it exists
-			if (getParameter("noauto") === null) {
+			var actions = desk.Actions.getInstance()
+			desk.Actions.init(afterActionsInitialized);
+
+			function afterActionsInitialized () {
+				// first try to automatically launch startup script if it exists
+				if (getParameter("noauto")) {
+					next();
+					return;
+				}
+				var initScript = 'code/init.js';
 				desk.FileSystem.exists(initScript, function (exists) {
 					if (exists) {
 						desk.FileSystem.executeScript(initScript);
 					} else {
-						Init();
+						next();
 					}
 				});
-			} else {
-				Init();
 			}
 
-			function Init() {
+			function next() {
 				var startupScript = getParameter("script");
 				if (startupScript) {
 					desk.FileSystem.executeScript(startupScript);
-				} else {
-					var actions = desk.Actions.getInstance()
-					actions.buildUI();
-					desk.Actions.init(function () {
-						var menu = new qx.ui.menu.Menu();
-						var button = new qx.ui.form.MenuButton("Places", null, menu);
-						qx.core.Init.getApplication().getRoot().add(button, {top : 0, left : 0});
-
-						function openFileBrowser (e) {
-							new desk.FileBrowser(e.getTarget().getLabel());
-						}
-
-						var dataDirs = actions.getSettings().dataDirs;
-						var dirs = Object.keys(dataDirs);
-                        dirs.sort();
-						for (var i = 0; i != dirs.length; i++) {
-							var dir = dirs[i];
-							var button = new qx.ui.menu.Button(dir);
-							button.addListener("execute", openFileBrowser);
-							menu.add(button);
-						}
-						new desk.FileBrowser(getParameter("rootDir"));
-					});
+					return;
 				}
+				actions.buildUI();
+				var menu = new qx.ui.menu.Menu();
+				var button = new qx.ui.form.MenuButton("Places", null, menu);
+				qx.core.Init.getApplication().getRoot().add(button, {top : 0, left : 0});
+
+				var dataDirs = actions.getSettings().dataDirs;
+				Object.keys(dataDirs).sort().forEach(function (dir) {
+					var button = new qx.ui.menu.Button(dir);
+					button.addListener("execute", function (e) {
+						new desk.FileBrowser(e.getTarget().getLabel());
+					});
+					menu.add(button);
+				});
+
+				new desk.FileBrowser(getParameter("rootDir"));
 			}
 		}
 	}
