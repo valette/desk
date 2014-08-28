32018796c91e8f801c681af3c0a796f065991f16
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2014-08-28 14:51:06 +0200

    cleanups

--- client/ext/three.js/VTKLoader.js
+++ client/ext/three.js/VTKLoader.js
@@ -45,76 +45,43 @@ THREE.VTKLoader.prototype = {
 	},
 
 	parse: function ( data ) {
-		var i;
+
+		var i, pattern, pattern2, result, nV, numV, numP;
 		var geometry = new THREE.Geometry();
-
-		function vertex( x, y, z ) {
-
-			geometry.vertices.push( new THREE.Vector3( x, y, z ) );
-
-		}
-
-		function face3( a, b, c ) {
-
-			geometry.faces.push( new THREE.Face3( a, b, c ) );
-
-		}
-
-		function face4( a, b, c, d ) {
-
-			geometry.faces.push( new THREE.Face4( a, b, c, d ) );
-
-		}
-
-		var pattern, pattern2, result;
 
 		pattern = /POINTS[\s]+([\d]+)/g;
 		result = pattern.exec( data );
-		var numV = parseInt(result[1], 10);
+		numV = parseInt(result[1], 10);
+		// float/int  float/int  float/int
 		pattern2 = /([\+|\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\+|\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\+|\-]?[\d]+[\.]?[\d|\-|e]*)/g;
 		pattern2.lastIndex = pattern.lastIndex;
 		
 
-		var nV = numV
-		// float/int  float/int  float/int
+		nV = numV
 		while ( nV--) {
 
 			result = pattern2.exec( data );
-
-			// ["1.0 2.0 3.0", "1.0", "2.0", "3.0"]
-
-			vertex( parseFloat( result[ 1 ] ), parseFloat( result[ 2 ] ), parseFloat( result[ 3 ] ) );
-
+			geometry.vertices.push( new THREE.Vector3(parseFloat( result[ 1 ] ),
+				parseFloat( result[ 2 ] ), parseFloat( result[ 3 ] ) )
+			);
 		}
 
 		pattern = /POLYGONS[\s]+([\d]+)[\s]([\d]+)/g;
 		result = pattern.exec( data );
-		var numP = parseInt(result[1], 10);
+		numP = parseInt(result[1], 10);
 
 		// 3 int int int
-
-		var pattern2 = /3[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)/g;
+		pattern2 = /3[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)/g;
 		pattern2.lastIndex = pattern.lastIndex;
 
 		for (i = 0; i < numP; i++) {
 
 			// ["3 1 2 3", "1", "2", "3"]
 			result = pattern2.exec( data );
-			face3( parseInt( result[ 1 ] ), parseInt( result[ 2 ] ), parseInt( result[ 3 ] ) );
+			geometry.faces.push( new THREE.Face3( parseInt( result[ 1 ] ),
+				parseInt( result[ 2 ] ), parseInt( result[ 3 ] ) ));
 
 		}
-
-		// 4 int int int int
-
-	/*	pattern = /4[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)/g;
-
-		while ( ( result = pattern.exec( data ) ) != null ) {
-
-			// ["4 1 2 3 4", "1", "2", "3", "4"]
-
-			face4( parseInt( result[ 1 ] ), parseInt( result[ 2 ] ), parseInt( result[ 3 ] ), parseInt( result[ 4 ] ) );
-
-		}*/
 
         pattern = /COLOR_SCALARS[\s][\x21-\x7E]+[\s]([\d]+)/g;
 		result = pattern.exec( data );
@@ -142,25 +109,31 @@ THREE.VTKLoader.prototype = {
         pattern = /POINT_DATA[\s]([\d]+)/g;
 		result = pattern.exec( data );
 		if (result) {
-            if ( numV === parseInt(result[1], 10)) {
-                pattern2 = /SCALARS[\s][\x21-\x7E]+[\s][\x21-\x7E]+/g;
-                pattern2.lastIndex = pattern.lastIndex;
-                result = pattern2.exec( data );
-                pattern =/LOOKUP_TABLE[\s][\x21-\x7E]+/;
-                pattern.lastIndex = pattern2.lastIndex;
-                result = pattern.exec( data );
-                pattern2 = /[\s]*([\-\+]?[0-9]*[\.]?[0-9]+)/g;
-                pattern2.lastIndex = pattern.lastIndex;
-                
-                var pointData = [];
-                for  (i = 0; i < numV; i++) {
-                    result = pattern2.exec( data );
-                    pointData.push(parseFloat(result[1], 10));
-                }
 
-                geometry.userData = geometry.userData || {};
-                geometry.userData.pointData = pointData;
-            }
+            if ( numV !== parseInt(result[1], 10)) {
+				return;
+			}
+
+			pattern2 = /SCALARS[\s][\x21-\x7E]+[\s][\x21-\x7E]+/g;
+			pattern2.lastIndex = pattern.lastIndex;
+			result = pattern2.exec( data );
+
+			pattern =/LOOKUP_TABLE[\s][\x21-\x7E]+/;
+			pattern.lastIndex = pattern2.lastIndex;
+			result = pattern.exec( data );
+
+			pattern2 = /[\s]*([\-\+]?[0-9]*[\.]?[0-9]+)/g;
+			pattern2.lastIndex = pattern.lastIndex;
+			
+			var pointData = [];
+			for  (i = 0; i < numV; i++) {
+				result = pattern2.exec( data );
+				pointData.push(parseFloat(result[1], 10));
+			}
+
+			geometry.userData = geometry.userData || {};
+			geometry.userData.pointData = pointData;
+
 		}
 
 		geometry.computeFaceNormals();
