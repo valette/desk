431a1cbf9e6fb03bde2c8f9ee7b905fda5224906
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2014-09-12 15:53:49 +0200

    ATTENTION : VTKLoader now outputs THREE.BufferGeometry

--- client/ext/three.js/VTKLoader.js
+++ client/ext/three.js/VTKLoader.js
@@ -47,7 +47,7 @@ THREE.VTKLoader.prototype = {
 	parse: function ( data ) {
 
 		var i, pattern, pattern2, result, nV, numV, numP;
-		var geometry = new THREE.Geometry();
+		var geometry = new THREE.BufferGeometry();
 
 		pattern = /POINTS[\s]+([\d]+)/g;
 		result = pattern.exec( data );
@@ -55,15 +55,16 @@ THREE.VTKLoader.prototype = {
 		// float/int  float/int  float/int
 		pattern2 = /([\+|\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\+|\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\+|\-]?[\d]+[\.]?[\d|\-|e]*)/g;
 		pattern2.lastIndex = pattern.lastIndex;
-		
 
-		nV = numV
+		var positions = new Float32Array( numV * 3 );
+
+		nV = numV;
+		i = 0;
 		while ( nV--) {
-
 			result = pattern2.exec( data );
-			geometry.vertices.push( new THREE.Vector3(parseFloat( result[ 1 ] ),
-				parseFloat( result[ 2 ] ), parseFloat( result[ 3 ] ) )
-			);
+			positions[i++] = parseFloat( result[ 1 ] );
+			positions[i++] = parseFloat( result[ 2 ] );
+			positions[i++] = parseFloat( result[ 3 ] );
 		}
 
 		pattern = /POLYGONS[\s]+([\d]+)[\s]([\d]+)/g;
@@ -74,36 +75,15 @@ THREE.VTKLoader.prototype = {
 		pattern2 = /3[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)/g;
 		pattern2.lastIndex = pattern.lastIndex;
 
-		for (i = 0; i < numP; i++) {
+		var indices = new Uint32Array( numP * 3 );
 
+		var j = 0;
+		for (i = 0; i < numP; i++) {
 			// ["3 1 2 3", "1", "2", "3"]
 			result = pattern2.exec( data );
-			geometry.faces.push( new THREE.Face3( parseInt( result[ 1 ] ),
-				parseInt( result[ 2 ] ), parseInt( result[ 3 ] ) ));
-
-		}
-
-        pattern = /COLOR_SCALARS[\s][\x21-\x7E]+[\s]([\d]+)/g;
-		result = pattern.exec( data );
-		if (result) {
-			pattern2 = /([\+|\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\+|\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\+|\-]?[\d]+[\.]?[\d|\-|e]*)/g;
-			pattern2.lastIndex = pattern.lastIndex;
-
-			if (parseInt(result[1], 10) === 3) {
-				var colors = [];
-				for  (i = 0; i < numV; i++) {
-					result = pattern2.exec( data );
-					var color = new THREE.Color( );
-					color.setRGB(result[1], result[2], result[3]);
-					colors.push(color);
-				}
-
-				geometry.faces.forEach(function (face) {
-					face.vertexColors.push(colors[face.a]);
-					face.vertexColors.push(colors[face.b]);
-					face.vertexColors.push(colors[face.c]);
-				});
-			}
+			indices[j++] = parseInt( result[ 1 ] );
+			indices[j++] = parseInt( result[ 2 ] );
+			indices[j++] = parseInt( result[ 3 ] );
 		}
 
         pattern = /POINT_DATA[\s]([\d]+)/g;
@@ -125,10 +105,10 @@ THREE.VTKLoader.prototype = {
 			pattern2 = /[\s]*([\-\+]?[0-9]*[\.]?[0-9]+)/g;
 			pattern2.lastIndex = pattern.lastIndex;
 			
-			var pointData = [];
+			var pointData = new Float32Array(numV);
 			for  (i = 0; i < numV; i++) {
 				result = pattern2.exec( data );
-				pointData.push(parseFloat(result[1], 10));
+				pointData[i] = parseFloat(result[1], 10);
 			}
 
 			geometry.userData = geometry.userData || {};
@@ -136,9 +116,10 @@ THREE.VTKLoader.prototype = {
 
 		}
 
-		geometry.computeFaceNormals();
+		geometry.addAttribute( 'index', new THREE.BufferAttribute( indices, 1 ) );
+		geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+
 		geometry.computeVertexNormals();
-		geometry.computeBoundingSphere();
 
 		return geometry ;
 	}
