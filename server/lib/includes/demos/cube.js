2a8d2f05719d707733c447dadc044ee8851628a6
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2014-12-02 14:32:51 +0100

    improve cube.js demo

--- server/lib/includes/demos/cube.js
+++ server/lib/includes/demos/cube.js
@@ -3,6 +3,11 @@ var view = new desk.MeshViewer();
 
 var geometry = new THREE.BoxGeometry( 200, 200, 200 );
 
-view.addGeometry( geometry , {label : "my cube"});
+var mesh = view.addGeometry( geometry , {label : "my cube"});
 view.getWindow().setCaption("my cube!");
+
+var edges = new THREE.WireframeHelper(mesh);
+edges.material.color.setRGB(0,0,0);
+mesh.add(edges);
+
 view.viewAll();
