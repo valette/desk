4d7219f738e552f1b6e4242cfd44c42651f99b24
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2016-12-09 19:21:01 +0100

    pimp my process name

--- proxy/deskSU.js
+++ proxy/deskSU.js
@@ -4,6 +4,7 @@ var user = process.env.DESK_USER;
 process.initgroups( user, user );
 process.setgid( user );
 process.setuid( user );
+process.title = 'desk';
 
 const script = process.env.DESK_STARTUP || path.join(__dirname, '../desk.js' );
 console.log( 'executing ' + script + ' as user ' + user );
