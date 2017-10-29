893872522a74d7212139b7765b9084a06842d2cc
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2017-10-29 14:59:51 +0100

    cleanup

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,129 +1,162 @@
-var	execSync  = require('child_process').execSync,
-	fs        = require('fs'),
-	http      = require('http'),
-	https     = require('https'),
-	httpProxy = require('http-proxy'),
-	os        = require('os');
+const execSync  = require( 'child_process' ).execSync,
+      fs        = require( 'fs' ),
+      http      = require( 'http' ),
+      https     = require( 'https' ),
+      httpProxy = require( 'http-proxy' ),
+      os        = require( 'os' );
 
-var	port      = 80,
-	port2     = 443;
+const port      = 80,
+      port2     = 443,
+      file       = __dirname + '/config.json',
+      keyFile    = __dirname + '/cert/privatekey.pem',
+      certFile   = __dirname + '/cert/certificate.pem',
+      caFile     = __dirname + '/cert/chain.pem';
 
-var defaultRoutes,
-	config     = __dirname + '/config.json',
-	ports      = __dirname + '/ports.json',
-	keyFile    = __dirname + '/cert/privatekey.pem',
-	certFile   = __dirname + '/cert/certificate.pem',
-	caFile     = __dirname + '/cert/chain.pem',
-	httpAllowed;
+let defaultRoutes,
+	httpAllowed,
+	routes; // main routing object
 
-var routes; // main routing object
-
-var proxy = httpProxy.createProxyServer( {
+const proxy = httpProxy.createProxyServer( {
 
 	xfwd : true,
 	agent : new http.Agent( { keepAlive: true } )
 
 } );
 
-proxy.on('error', function(err, req, res) {
-	//just end the request
-	res.end();
-});
+proxy.on('error', ( err, req, res ) => res.end() );
 
-var serverOpts = {
-	key: fs.readFileSync(keyFile, 'utf8'),
-	cert: fs.readFileSync(certFile, 'utf8'),
+const serverOpts = {
+
+	key: fs.readFileSync( keyFile, 'utf8' ),
+	cert: fs.readFileSync( certFile, 'utf8' ),
+
 };
 
 if ( fs.existsSync( caFile ) ) {
-	serverOpts.ca = fs.readFileSync(caFile, 'utf8')
+
+	serverOpts.ca = fs.readFileSync( caFile, 'utf8' )
+
 } else {
+
 	console.warn( 'no .ca file provided' );
+
 }
 
-var proxyServer = https.createServer( serverOpts, processRequest);
+const proxyServer = https.createServer( serverOpts, processRequest );
 
-var server = http.createServer(function (req, res) {
-	if (httpAllowed[req.headers.host]) {
+const server = http.createServer( function ( req, res ) {
+
+	if ( httpAllowed[ req.headers.host ] ) {
+
 		processRequest(req, res);
-	} else {
-		res.writeHead(302, {Location: 'https://' + req.headers.host + req.url});
-		res.end();
-	}
-});
 
-function processRequest (req, res) {
-	if (req.url == "/" && defaultRoutes[req.headers.host] ) {
-		res.writeHead( 302, {Location: defaultRoutes[req.headers.host]} );
-		res.end();
 	} else {
-		var target = routes[req.headers.host + '/' + req.url.split("/")[1]];
-		if (!target)  {
-			res.writeHead(404, {"Content-Type": "text/plain"});
-			res.write("404 Not Found\n");
+
+		res.writeHead( 302, { Location: 'https://' + req.headers.host + req.url } );
+		res.end();
+
+	}
+
+} );
+
+function processRequest ( req, res ) {
+
+	if ( req.url == "/" && defaultRoutes[ req.headers.host ] ) {
+
+		res.writeHead( 302, { Location: defaultRoutes[ req.headers.host ] } );
+		res.end();
+
+	} else {
+
+		const target = routes[ req.headers.host + '/' + req.url.split( "/" )[ 1 ] ];
+
+		if ( !target )  {
+
+			res.writeHead( 404, { "Content-Type": "text/plain" } );
+			res.write( "404 Not Found\n" );
 			res.end();
 			return;
+
 		}
-		proxy.web(req, res, target);
+
+		proxy.web( req, res, target );
+
 	}
 }
 
-proxyServer.on('upgrade', function (req, socket, head) {
-	var target = routes[req.headers.host + '/' + req.url.split("/")[1]];
-	if (!target)  {
-		res.end();
-		return;
-	}
-	proxy.ws(req, socket, head, target);
-});
+proxyServer.on( 'upgrade', function ( req, socket, head ) {
+
+	const target = routes[ req.headers.host + '/' + req.url.split( "/" )[ 1 ] ];
+	if ( !target ) return res.end();
+	proxy.ws( req, socket, head, target );
+
+} );
 
 updateRoutes();
 
 // start proxies
 server.listen( port, function () {
+
 	proxyServer.listen( port2, function () {
-		console.log('desk-http2https service listening on port ' + port);
-		console.log('desk-proxy service listening on port ' + port2);
-		process.setgid('dproxy');
-		process.setuid('dproxy');
-	});
-});
+
+		console.log( 'desk-http2https service listening on port ' + port );
+		console.log( 'desk-proxy service listening on port ' + port2 );
+		process.setgid( 'dproxy' );
+		process.setuid( 'dproxy' );
+
+	} );
+
+} );
 
 function updateRoutes() {
+
 	try{
-		var routesContent = JSON.parse(fs.readFileSync(config));
+
+		const newRoutes = JSON.parse( fs.readFileSync( file ) );
 		routes = {};
 
-		var baseURL = routesContent.baseURL || os.hostname();
+		const baseURL = newRoutes.baseURL || os.hostname();
 
-		routesContent.users.forEach( function ( user ) {
+		for( let user of newRoutes.users ) {
+
 			routes[ baseURL + '/' + user ] = {
-				target : 'http://' + baseURL + ':' + routesContent.ports[user]
+
+				target : 'http://' + baseURL + ':' + newRoutes.ports[ user ]
+
 			};
-		});
+
+		}
 
 		// add external proxies
-		var otherRoutes = routesContent.otherRoutes || {};
-		Object.keys( otherRoutes ).forEach(function (key) {
-			routes[ key ] = { target : otherRoutes[ key ] };
-		});
-		defaultRoutes = routesContent.defaultRoutes;
-		httpAllowed = routesContent.httpAllowed || {}
+		const otherRoutes = newRoutes.otherRoutes || {};
+
+		for ( let [ key, value ] of Object.entries( otherRoutes ) ) {
+
+			routes[ key ] = { target : value };
+
+		}
+
+		defaultRoutes = newRoutes.defaultRoutes;
+		httpAllowed = newRoutes.httpAllowed || {}
 		console.log( '... done! Routes:' );
 		console.log( routes );
+
 	} catch (err) {
+
 		console.log( "error updating routes : " );
 		console.log( err );
+
 	}
 }
 
 // watch routes files for auto-update
-fs.watchFile(config, function (curr, prev) {
-	if (curr.mtime > prev.mtime) {
-		console.log(new Date().toDateString() + " " + new Date().toTimeString());
-		console.log(config + ' modified, updating routes...');
-		updateRoutes();
-	}
-});
+fs.watchFile( file, function ( curr, prev ) {
 
-console.log('Watching file ' + config + ' for routes');
+	if ( curr.mtime <= prev.mtime ) return;
+	console.log( new Date().toDateString() + " " + new Date().toTimeString() );
+	console.log( file + ' modified, updating routes...' );
+	updateRoutes();
+
+} );
+
+console.log('Watching file ' + file + ' for routes');
