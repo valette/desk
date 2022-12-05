436aef7619fabfdc29a6deae544626455473b055
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2022-12-05 13:13:28 +0100

    more cubes in cube.js

--- extensions/demos/cube.js
+++ extensions/demos/cube.js
@@ -1,11 +1,44 @@
  
 const viewer = new desk.MeshViewer();
 
-const geometry = new THREE.BoxGeometry( 200, 200, 200 );
-const mesh = viewer.addGeometry( geometry , {label : "my cube"});
 viewer.getWindow().setCaption("my cube!");
+const size = 40;
 
-const edgeGeometry = new THREE.WireframeGeometry( geometry );
-const edges = new THREE.LineSegments( edgeGeometry );
-edges.material.color.setRGB(0,0,0);
-viewer.addMesh(  edges );
+for ( let i = 0; i < size; i++ ) {
+
+        for ( let j = 0; j < size; j++ ) {
+            const geometry = new THREE.BoxGeometry( 200, 200, 200 );
+            const mesh = viewer.addGeometry( geometry , {label : "my cube"});
+            const edgeGeometry = new THREE.WireframeGeometry( geometry );
+            const edges = new THREE.LineSegments( edgeGeometry );
+            edges.material.color.setRGB(0,0,0);
+            viewer.addMesh(  edges );
+            mesh.position.set( i * 400, j * 400, 0 );
+            edges.position.set( i * 400, j * 400, 0 );
+
+    }
+
+}
+
+viewer.resetView();
+const button = new qx.ui.form.Button( "start" );
+viewer.add( button, { left : 10, bottom : 50 } );
+button.addListener( "execute", run );
+const label = new qx.ui.basic.Label( "" );
+viewer.add( label, { left : 10, bottom : 10 } );
+
+async function run() {
+
+    button.setEnabled( false );
+    const start = performance.now();
+    const n = 50;
+    for ( let i = 0; i < n ; i++)
+        await viewer.renderAsync();
+
+    const stop = performance.now();
+    const duration = Math.round( ( stop - start ) / n );
+    label.setValue( duration + "ms per frame");
+    button.setEnabled( true );
+
+}
+
